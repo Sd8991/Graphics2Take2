20c8059b47773560eb32899c6896892716547327
Sjors Derksen <sjors.derksen@hotmail.nl>
2018-05-19 21:17:29 +0200

    Added references to allow for writing camera movement

--- Application.cs
+++ Application.cs
@@ -3,7 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 class Application
 {
+    public Application() { }
+
+    public void moveCamera(Camera c)
+    {
+        
+    }
+
+    public void rotateCamera(Camera c)
+    {
+
+    }
+
+
 }
